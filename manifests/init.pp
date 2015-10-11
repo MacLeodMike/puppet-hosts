6dc85810cc042d82d8d21f7865e24117a398aca5
Mike MacLeod <mikemacleod@gmail.com>
2015-10-11 17:00:00 -0400

    Fixups

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,23 +10,19 @@ class hosts (
   $localhost_aliases = $hosts::params::localhost_aliases,
   $localhost6_aliases = $hosts::params::localhost6_aliases,
   $fqdn_aliases = $hosts::params::fqdn_aliases,
-  $purge_hosts = $hosts::params::purge_hosts,
-  $hosts_file = $hosts::params::hosts_file,
 
 ) inherits hosts::params {
 
-  validate_boolean($enable_ipv4_localhost)
-  validate_boolean($enable_ipv6_localhost)
-  validate_boolean($enable_fqdn_entry)
-  validate_boolean($use_fqdn)
+  validate_bool($enable_ipv4_localhost)
+  validate_bool($enable_ipv6_localhost)
+  validate_bool($enable_fqdn_entry)
+  validate_bool($use_fqdn)
   validate_string($localhost)
   validate_string($localhost6)
   validate_string($fqdn)
   validate_array($localhost_aliases)
   validate_array($localhost6_aliases)
   validate_array($fqdn_aliases)
-  validate_boolean($purge_hosts)
-  validate_string($hosts_file)
 
   anchor { 'puppet::begin': } ->
   class { '::puppet::config': } ->
